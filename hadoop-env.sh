28c1262be976981cb2bca1abc6489211e2e615aa
Cecile Liu <ager89@yahoo.com.tw>
2018-02-24 15:23:56 +0800

    Create hadoop-env.sh

---
+++ hadoop-env.sh
@@ -0,0 +1,2 @@
+change the 'export JAVA_HOME=${JAVA_HOME} into 'export JAVA_HOME=/usr/lib/jvm/java8/openjdk/amd64'  
+/usr/lib/jvm/java8/openjdk/amd64 is my java folder location, you should type yours
